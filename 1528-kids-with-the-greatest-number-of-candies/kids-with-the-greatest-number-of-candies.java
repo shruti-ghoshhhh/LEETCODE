71902f500110ebfe72f9b0f301429aa73ff18de7
Shruti Ghosh <shrutighosh958@gmail.com>
2025-07-01 21:44:17 +0530

    Time: 1 ms (83.67%) | Memory: 42.9 MB (7.39%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -1,19 +1,14 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max=Integer.MIN_VALUE;
         ArrayList<Boolean> list=new ArrayList<>();
-        for(int i=0; i<candies.length;i++){
-                int max=candies[i]+extraCandies;
-                int temp=max;
-            for(int j=0; j<candies.length;j++){
-                if(candies[j]>=max) max=candies[j];
-            }
-            if(temp==max){
-                list.add(true);
-            }
-            else{
-                list.add(false);
-            }
+        for(int candy:candies){
+            if(max<candy) max=candy;
+        }
 
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies>=max) list.add(true);
+            else list.add(false);
         }
         return list;
     }
