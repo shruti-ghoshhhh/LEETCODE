028825bc176df124b83adcf41195927919987380
Shruti Ghosh <shrutighosh958@gmail.com>
2025-07-04 21:45:03 +0530

    Time: 9 ms (23.06%) | Memory: 40.5 MB (49.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,6 @@
 class Solution {
     public int fib(int n) {
         if(n<=1) return n;
-        int last=fib(n-1);
-        int slast=fib(n-2);
-        return last+slast;
+        return fib(n-1)+fib(n-2);
     }
 }
