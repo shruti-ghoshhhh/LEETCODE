580f4c9ee71d21d8ebae479e2f4afd7cead970f0
Shruti Ghosh <shrutighosh958@gmail.com>
2025-07-06 16:48:35 +0530

    Time: 1 ms (99.72%) | Memory: 42 MB (46.64%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,8 +1,10 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char result = 0;
-        for (char c : s.toCharArray()) result ^= c;
-        for (char c : t.toCharArray()) result ^= c;
-        return result;
+        int count[]=new int[26];
+        for (char c : s.toCharArray()) count[c-'a']++;
+        for (char c : t.toCharArray()) {
+            if(--count[c-'a']<0) return c;
     }
+    return ' ';
+}
 }
